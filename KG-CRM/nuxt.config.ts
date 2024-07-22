ee0cec697d371380db91b4f658cbab5a6c7e36b8
Artem Sadikov <lolyoufun2@gmail.com>
2024-07-22 18:06:36 +0300

    test

--- KG-CRM/nuxt.config.ts
+++ KG-CRM/nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@prisma/nuxt",
