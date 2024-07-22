53bbcdfaca20cd80fef5648172f8be7f3b65f26e
Artem Sadikov <lolyoufun2@gmail.com>
2024-07-22 18:24:29 +0300

    test

--- KG-CRM/pages/EmployerAuth.vue
+++ KG-CRM/pages/EmployerAuth.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
-import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-const layout = "default";
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as z from "zod";
+
+const formSchema = toTypedSchema(
+  z.object({
+    username: z.string().min(2).max(50),
+  })
+);
+
+const form = useForm({
+  validationSchema: formSchema,
+});
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log("Form submitted!", values);
+});
 </script>
 
 <template>
-  <NuxtLayout :name="layout">
-    <Form>
-      <FormField v-slot="">
-        <FormItem>
-          <FormLabel />
-          <FormControl>
-            <!-- any Form Input component or native input elements -->
-          </FormControl>
-          <FormDescription />
-          <FormMessage />
-        </FormItem>
-      </FormField>
-    </Form>
-  </NuxtLayout>
+  <form @submit="onSubmit">
+    <FormField v-slot="{ componentField }" name="username">
+      <FormItem>
+        <FormLabel>Username</FormLabel>
+        <FormControl>
+          <Input type="text" placeholder="shadcn" v-bind="componentField" />
+        </FormControl>
+        <FormDescription> This is your public display name. </FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+    <Button type="submit"> Submit </Button>
+  </form>
 </template>
