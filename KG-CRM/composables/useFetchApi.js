3af927bd5d430e150d3fdad9d6cabb65b6c56949
FoxM15H <h1sha.fox@gmail.com>
2024-07-23 21:10:34 +0300

    Хуита

--- KG-CRM/composables/useFetchApi.js
+++ KG-CRM/composables/useFetchApi.js
@@ -1,10 +1,19 @@
 export default (url, options = {}) => {
-  const { useAuthToken } = useAuth();
-  return $fetch(url, {
-    ...options,
-    headers: {
-      ...options.headers,
-      Authorization: `Bearer ${useAuthToken().value}`,
-    },
-  });
+  try {
+    const { useAuthToken } = useAuth();
+
+    const authTokenValue = useAuthToken().value;
+
+    return $fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: authTokenValue ? `Bearer ${authTokenValue}` : "",
+      },
+    });
+  } catch (error) {
+    console.error("Ошибка получения токена авторизации:", error);
+    // Обработка ошибки или возврат значения по умолчанию
+    return $fetch(url, options);
+  }
 };
