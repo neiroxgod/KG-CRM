f6c7e2330378fa9e1517a7d502ceb5fe12b01a58
Artem Sadikov <lolyoufun2@gmail.com>
2024-07-28 02:18:33 +0300

    be

--- KG-CRM/components/widgets/modules/table/dropdown.vue
+++ KG-CRM/components/widgets/modules/table/dropdown.vue
@@ -15,33 +15,20 @@ defineProps<{
     id: string;
   };
 }>();
-
-defineEmits<{
-  (e: "expand"): void;
-}>();
-
-function copy(id: string) {
-  navigator.clipboard.writeText(id);
-}
 </script>
 
 <template>
   <DropdownMenu>
     <DropdownMenuTrigger as-child>
       <Button variant="ghost" class="w-8 h-8 p-0">
-        <span class="sr-only">Open menu</span>
+        <span class="sr-only">Открыть меню</span>
         <MoreHorizontal class="w-4 h-4" />
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent align="end">
-      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-      <DropdownMenuItem @click="copy(payment.id)">
-        Copy payment ID
-      </DropdownMenuItem>
-      <DropdownMenuItem @click="$emit('expand')"> Expand </DropdownMenuItem>
+      <DropdownMenuItem> Редактировать </DropdownMenuItem>
       <DropdownMenuSeparator />
-      <DropdownMenuItem>View customer</DropdownMenuItem>
-      <DropdownMenuItem>View payment details</DropdownMenuItem>
+      <DropdownMenuItem class="text-red-500"> Удалить </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
 </template>
--- KG-CRM/pages/admin/employers.vue
+++ KG-CRM/pages/admin/employers.vue
@@ -1,28 +1,132 @@
 <template>
   <div class="p-5">
     <div class="flex flex-row-reverse">
-      <SharedAddButton label="Добавить" />
+      <Form
+        v-slot="{ submitForm }"
+        as=""
+        :validation-schema="formSchema"
+        @submit="onSubmit"
+      >
+        <Dialog>
+          <DialogTrigger as-child>
+            <SharedAddButton />
+          </DialogTrigger>
+          <DialogContent class="w-full">
+            <DialogHeader>
+              <DialogTitle>Создание сотрудника</DialogTitle>
+            </DialogHeader>
+            <Separator />
+            <form @submit="submitForm">
+              <div class="flex justify-between gap-4">
+                <FormField v-slot="{ componentField }" name="name">
+                  <FormItem class="w-full">
+                    <FormLabel>ФИО</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Иванов Иван Иванович"
+                        v-bind="componentField"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </FormField>
+
+                <FormField v-slot="{ componentField }" name="phone">
+                  <FormItem class="w-full">
+                    <FormLabel>Телефон</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="phone"
+                        placeholder="+7 999 999 99 99"
+                        v-bind="componentField"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </FormField>
+              </div>
+              <div class="flex justify-between">
+                <FormField v-slot="{ componentField }" name="email">
+                  <FormItem class="w-full">
+                    <FormLabel>Почта</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="test@mail.ru"
+                        v-bind="componentField"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </FormField>
+              </div>
+              <div class="flex justify-between gap-4">
+                <FormField v-slot="{ componentField }" name="username">
+                  <FormItem class="w-full">
+                    <FormLabel>Логин к CRM</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="test@mail.ru"
+                        v-bind="componentField"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </FormField>
+
+                <FormField v-slot="{ componentField }" name="password">
+                  <FormItem class="w-full">
+                    <FormLabel>Пароль к CRM</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="test@mail.ru"
+                        v-bind="componentField"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                </FormField>
+              </div>
+            </form>
+            <Separator />
+            <DialogFooter>
+              <Button type="submit" class="bg-btnPrimary" form="dialogForm">
+                Добавить
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </Form>
     </div>
     <div class="mt-5">
-      <DataTable :columns="columns" :data="data.employers" />
+      <DataTable
+        v-if="data.employers"
+        :columns="columns"
+        :data="data.employers"
+      />
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { columns } from "@/components/widgets/modules/table/columns";
 import DataTable from "@/components/widgets/modules/table/data.vue";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as z from "zod";
+import { toast } from "@/components/ui/toast";
 
-const data = ref([]);
-const userStore = useAuthStore(); //benis
-
+const data = ref<any>([]);
+const userStore = useAuthStore();
 async function getData() {
   const fetchApi = useFetchApi(userStore.token);
-  let employers = ref([]);
+  let employers = ref<any>([]);
   employers.value = await fetchApi("/api/employers/list", {
     method: "POST",
     body: {
-      accountId: userStore.user.accountId,
+      accountId: userStore.user!.accountId,
     },
   });
 
@@ -32,6 +136,28 @@ async function getData() {
 onMounted(async () => {
   data.value = await getData();
 });
+
+const formSchema = toTypedSchema(
+  z.object({
+    username: z.string().min(2, "Введите логин"),
+    name: z.string().min(2, "Введите ФИО"),
+    email: z.string().email("Укажите корректную почту"),
+    phone: z.string().min(10, "Укажите телефон"),
+    password: z.string().min(6, "Пароль должен содержать минимум 6 символов"),
+  })
+);
+
+function onSubmit(values: any) {
+  console.log("submited:", values);
+  toast({
+    title: "You submitted the following values:",
+    description: h(
+      "pre",
+      { class: "mt-2 w-[340px] rounded-md bg-slate-950 p-4" },
+      h("code", { class: "text-white" }, JSON.stringify(values, null, 2))
+    ),
+  });
+}
 </script>
 
 <style></style>
