cef1d38faadc4581df0c06407f14ac17be0433ac
Artem Sadikov <lolyoufun2@gmail.com>
2024-09-14 00:53:50 +0300

    tasks

--- KG-CRM/components/widgets/modules/tasks/list.vue
+++ KG-CRM/components/widgets/modules/tasks/list.vue
@@ -2,12 +2,18 @@
   <div
     v-for="task in tasks"
     :key="task.id"
-    class="border-2 mt-2 w-full rounded-md bg-white dark:bg-slate-900 transition-all duration-300"
+    class="border-2 mt-2 w-full rounded-md bg-white dark:bg-slate-900 dark:border-slate-600 transition-all duration-300"
   >
-    <div v-if="task.timedeadline">
+    <div
+      @dblclick="toggleTask(task.id as number)"
+      class="cursor-pointer"
+      v-if="task.timedeadline"
+    >
       <div class="flex items-center justify-between">
         <div class="flex items-center">
-          <div class="w-32 mx-5 p-2 text-center font-inter text-slate-600">
+          <div
+            class="w-32 mx-5 p-2 text-center font-inter text-slate-600 dark:text-white"
+          >
             <div class="text-md font-light">
               {{
                 new Date(task.timedeadline).toLocaleString("ru-RU", {
@@ -65,14 +71,62 @@
             </div>
           </div>
         </div>
-        <div class="mr-5">
-          <Button @click="toggleTask(task.id as number)" variant="outline"
-            >Редактировать</Button
+        <div class="mr-5 flex gap-2">
+          <Badge
+            v-if="task.result"
+            class="px-5 rounded-md font-inter text-white bg-lime-500 dark:text-white dark:bg-green-400"
           >
+            Закрыта
+          </Badge>
+          <Badge
+            v-else-if="task.timedeadline && !task.result"
+            class="px-5"
+            :class="{
+              'font-inter rounded-md text-white bg-red-400 dark:text-white dark:bg-red-600':
+                new Date(task.timedeadline) <
+                new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+              'font-inter rounded-md text-white bg-yellow-500 dark:text-white dark:bg-yellow-500':
+                new Date(task.timedeadline) >
+                new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+              'font-inter rounded-md text-white bg-green-400 dark:text-white dark:bg-green-400':
+                new Date(task.timedeadline) >
+                new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            }"
+          >
+            {{
+              new Date(task.timedeadline) <
+              new Date(Date.now() + 3 * 24 * 60 * 60 * 1000)
+                ? "Уже просрочена"
+                : new Date(task.timedeadline) <
+                    new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+                  ? "Осталось менее 7 дней"
+                  : "До дедлайна более 7 дней"
+            }}
+          </Badge>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+              <Button variant="ghost" class="w-8 h-8 p-0">
+                <span class="sr-only">Открыть меню</span>
+                <Icon class="h-4 w-4" name="material-symbols:more-vert" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem @click="toggleTask(task.id as number)"
+                >Редактировать</DropdownMenuItem
+              >
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                @click="deleteTask(task.id as number)"
+                class="text-red-500"
+                >Удалить</DropdownMenuItem
+              >
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
-      <div v-show="openTaskId === task.id" class="flex flex-col gap-2 p-2">
-        <SharedTextareaWithLabel
+      <div v-show="openTaskId === task.id" class="p-2">
+        <SharedInputWithLabel
           v-model:model-value="task.text"
           label="Текст задачи"
           placeholder="Например: 'Собрать подписи с родителей.'"
@@ -83,31 +137,37 @@
           label="Результат выполнения"
           placeholder="Например: 'Собрал подписи с родителей.'"
         />
+        <div class="flex gap-2 lg:flex-nowrap flex-wrap">
+          <SharedSelectWithLabel
+            v-if="usersWithoutRelations"
+            v-model:model-value="task.user.id"
+            :items="usersWithoutRelations"
+            label="Ответственный сотрудник"
+          />
 
-        <SharedSelectWithLabel
-          v-if="usersWithoutRelations"
-          v-model:model-value="task.user.id"
-          :items="usersWithoutRelations"
-          label="Ответственный сотрудник"
-        />
+          <SharedSelectWithLabel
+            v-if="taskTypes"
+            v-model:model-value="task.taskType"
+            :items="taskTypes"
+            label="Тип задачи"
+          />
 
-        <SharedSelectWithLabel
-          v-if="taskTypes"
-          v-model:model-value="task.taskType"
-          :items="taskTypes"
-          label="Тип задачи"
-        />
+          <SharedSelectWithLabel
+            v-if="usersWithoutRelations"
+            v-model:model-value="task.targetUserId"
+            :items="usersWithoutRelations"
+            label="Объект задачи"
+          />
+          <div class="w-full mt-2">
+            <Label class=""> Срок до </Label>
+            <SharedDatePicker v-model:model-value="task.timedeadline" />
+          </div>
+        </div>
 
-        <SharedSelectWithLabel
-          v-if="usersWithoutRelations"
-          v-model:model-value="task.targetUserId"
-          :items="usersWithoutRelations"
-          label="Объект задачи"
-        />
-        <Label> Срок до </Label>
-        <SharedDatePicker v-model:model-value="task.timedeadline" />
         <!-- Добавьте другие поля для редактирования -->
-        <Button variant="outline" @click="saveTask(task.id)">Сохранить</Button>
+        <Button class="w-full mt-2" variant="outline" @click="saveTask(task.id)"
+          >Сохранить</Button
+        >
       </div>
     </div>
   </div>
@@ -130,6 +190,17 @@ const props = defineProps<{
 }>();
 
 const openTaskId = ref<number | null>(null);
+
+const deleteTask = async (taskId: number) => {
+  await CRM_API_INSTANCE.tasks.delete(taskId);
+  listStore.removeFromList(taskId);
+
+  toast({
+    title: "Задача удалена",
+    duration: 2000,
+    variant: "success",
+  });
+};
 
 const toggleTask = (id: number) => {
   openTaskId.value = openTaskId.value === id ? null : id;
