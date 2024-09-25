8db680cb8cb0cc99d50156011377f03c1bdc2941
Artem Sadikov <lolyoufun2@gmail.com>
2024-09-25 11:02:08 +0300

    test

--- crm_back/microservices/crm/src/accounts/accounts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AccountsService } from './accounts.service';
-import { AccountsController } from './accounts.controller';
-
-@Module({
-  providers: [AccountsService],
-  controllers: [AccountsController]
-})
-export class AccountsModule {}
--- crm_back/microservices/crm/src/auth/createUser.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateUserDto {
-  name: string;
-  username: string;
-  phone: string;
-  password: string;
-  email: string;
-}
